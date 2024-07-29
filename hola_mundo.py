4ad6d232c3a537d2b7ebc440802b037ae2553567
ulises-lopez-diaz <69325727+ulises-lopez-diaz@users.noreply.github.com>
2024-07-29 02:21:34 -0500

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     nombre = os.getenv("USERNAME")
-    print(f"¡Hola, {nombre} desde GitHub!")
+    print(f"¡Hola, {nombre} desde GitHub!!")
 
 
 if __name__ == "__main__":
